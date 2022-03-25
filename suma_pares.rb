614143027e9cb9f9b0764e125cb3284207442f7b
JosephBG25 <josephbustamanteg@gmail.com>
2022-03-25 20:29:21 -0300

    Finishes: suma_pares.rb

---
+++ suma_pares.rb
@@ -0,0 +1,16 @@
+# Crea un programa llamado suma_pares.rb que sume los primeros n números pares, donde
+# n es ingresado por el usuario por línea de comandos.
+# Tip: El cero no es par, no afecta en la suma pero tenemos que tener cuidado con
+# los bordes del ciclo.
+# Uso:
+# ruby suma_pares.rb 20
+# 420
+
+num = ARGV[0].to_i
+suma = 0
+num.times do |i|
+    i=(i+1)*2
+    suma += i
+end
+
+puts suma
